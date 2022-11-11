96c0cf1a5eb097e79e37808acd2a10ca9712d1fc
MichaHornung <michaelhornung2022@icloud.com>
2022-11-11 09:25:51 +0100

    Initial Commit

---
+++ 11.11.2022 Praxisfreitag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  11.11.2022 Praxisfreitag
+//
+//  Created by Michael Hornung on 11.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
